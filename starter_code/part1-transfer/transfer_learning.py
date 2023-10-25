0c99fe09e4d42c6765ba6a7e31ef2e2acad83b2b
Christopher Phillips <chris@christopher-phillips.com>
2023-10-25 19:29:23 -0400

    Small Dataset Training
    Initial code work for Small Dataset training using Torch and Transfer Learning

--- starter_code/part1-transfer/transfer_learning.py
+++ starter_code/part1-transfer/transfer_learning.py
@@ -5,42 +5,236 @@
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
+from torch import optim, cuda
 from torch.optim import lr_scheduler
 from torchvision import datasets, transforms 
 from TrainModel import train_model
 from TestModel import test_model
 from torchvision import models
+from torch.utils.data import DataLoader
+# Useful for examining network
+#from torchsummary import summary
+# Timing utility
+from timeit import default_timer as timer
+import numpy as np
+import pandas as pd
+import os
 
+# Visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline
+
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+# Image manipulations
+from PIL import Image
 
 # use this mean and sd from torchvision transform documentation
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 
-#Set up Transforms (train, val, and test)
+
 
 #<<<YOUR CODE HERE>>>
+DATA_DIR = "imagedata-50/"
+TRAIN_DATA_DIR = DATA_DIR+"train/"
+VALID_DATA_DIR = DATA_DIR+"val/"
+TEST_DATA_DIR = DATA_DIR+"test/"
+save_file_name = 'vgg16-transfer.pt'
+checkpoint_path = 'vgg16-transfer.pth'
+categories=[]
+img_categories = []
+n_train = []
+n_valid = []
+n_test = []
+hs = []
+ws = []
 
+# Iterate through each category
+for d in os.listdir(TRAIN_DATA_DIR):
+    categories.append(d)
+    
+    # Number of each image
+    train_imgs = os.listdir(TRAIN_DATA_DIR + d)
+    valid_imgs = os.listdir(VALID_DATA_DIR + d)
+    test_imgs = os.listdir(TEST_DATA_DIR + d)
+    n_train.append(len(train_imgs))
+    n_valid.append(len(valid_imgs))
+    n_test.append(len(test_imgs))
 
+    # Find stats for train images
+    for i in train_imgs:
+        img_categories.append(d)
+        img = Image.open(TRAIN_DATA_DIR + d + '/' + i)
+        img_array = np.array(img)
+        # Shape
+        hs.append(img_array.shape[0])
+        ws.append(img_array.shape[1])
 
+# Dataframe of categories
+cat_df = pd.DataFrame({'category': categories,
+                       'n_train': n_train,
+                       'n_valid': n_valid, 'n_test': n_test}).\
+    sort_values('category')
+
+# Dataframe of training images
+image_df = pd.DataFrame({
+    'category': img_categories,
+    'height': hs,
+    'width': ws
+})
+
+cat_df.sort_values('n_train', ascending=False, inplace=True)
+print(cat_df.head())
+
+#distribution of image sizes
+img_dsc = image_df.groupby('category').describe()
+print(img_dsc.head())
 
 #Set up DataLoaders (train, val, and test)
 batch_size = 10
 num_workers = 4
 
+# Whether to train on a gpu
+train_on_gpu = cuda.is_available()
+print(f'Train on gpu: {train_on_gpu}')
+
+
 #<<<YOUR CODE HERE>>>
+# Image transformations
+image_transforms = {
+    # Train uses data augmentation
+    'train':
+    transforms.Compose([
+        transforms.RandomResizedCrop(size=200, scale=(0.8, 1.0)),
+        transforms.RandomRotation(degrees=30),
+        transforms.ColorJitter(),
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(size=64),  # Image net standards
+        transforms.ToTensor(),
+        transforms.Normalize(mean,
+                             std)  # Imagenet standards
+    ]),
+    # Validation does not use augmentation
+    'val':
+    transforms.Compose([
+        transforms.Resize(size=64),
+        transforms.CenterCrop(size=180),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    # Test does not use augmentation
+    'test':
+    transforms.Compose([
+        transforms.Resize(size=64),
+        transforms.CenterCrop(size=180),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+}
 
 #hint, create a variable that contains the class_names. You can get them from the ImageFolder
+# Datasets from each folder
+data = {
+    'train':
+    datasets.ImageFolder(root=TRAIN_DATA_DIR, transform=image_transforms['train']),
+    'val':
+    datasets.ImageFolder(root=VALID_DATA_DIR, transform=image_transforms['val']),
+    'test':
+    datasets.ImageFolder(root=TEST_DATA_DIR, transform=image_transforms['test'])
+}
+
+# Dataloader iterators
+dataloaders = {
+    'train': DataLoader(data['train'], batch_size=batch_size, shuffle=True),
+    'val': DataLoader(data['val'], batch_size=batch_size, shuffle=True),
+    'test': DataLoader(data['test'], batch_size=batch_size, shuffle=True)
+}
+
+trainiter = iter(dataloaders['train'])
+features, labels = next(trainiter)
+features.shape, labels.shape
+
+n_classes = len(cat_df)
+print(f'There are {n_classes} different classes.')
+
+print(len(data['train'].classes))
 
 
 
+#Set up Transforms (train, val, and test)
 # Using the VGG16 model for transfer learning 
 # 1. Get trained model weights
 # 2. Freeze layers so they won't all be trained again with our data
 # 3. Replace top layer classifier with a classifer for our 3 categories
 
 #<<<YOUR CODE HERE>>>
+model = models.vgg16(pretrained=True)
+model
 
+# Freeze early layers
+for param in model.parameters():
+    param.requires_grad = False
+    
+n_inputs = model.classifier[6].in_features
+
+# Add on classifier
+model.classifier[6] = nn.Sequential(
+    nn.Linear(n_inputs, 256), nn.ReLU(), nn.Dropout(0.4),
+    nn.Linear(256, n_classes), nn.LogSoftmax(dim=1))
+
+model.classifier
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f'{total_params:,} total parameters.')
+total_trainable_params = sum(
+    p.numel() for p in model.parameters() if p.requires_grad)
+print(f'{total_trainable_params:,} training parameters.')    
+
+
+def get_pretrained_model(model_name):
+
+    if model_name == 'vgg16':
+        model = models.vgg16(pretrained=True)
+
+        # Freeze early layers
+        for param in model.parameters():
+            param.requires_grad = False
+        n_inputs = model.classifier[6].in_features
+
+        # Add on classifier
+        model.classifier[6] = nn.Sequential(
+            nn.Linear(n_inputs, 256), nn.ReLU(), nn.Dropout(0.2),
+            nn.Linear(256, n_classes), nn.LogSoftmax(dim=1))
+
+    # Move to gpu and parallelize
+    if train_on_gpu:
+        model = model.to('cuda')
+
+    return model
+model = get_pretrained_model('vgg16')
+#summary(model, input_size=(3, 224, 224), batch_size=batch_size, device='cuda')
+
+print(model.classifier[6])
+
+model.class_to_idx = data['train'].class_to_idx
+model.idx_to_class = {
+    idx: class_
+    for class_, idx in model.class_to_idx.items()
+}
+
+list(model.idx_to_class.items())[:10]
+
+
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters())
+
+for p in optimizer.param_groups[0]['params']:
+    if p.requires_grad:
+        print(p.shape)
+        
 # Train model with these hyperparameters
 # 1. num_epochs 
 # 2. criterion 
@@ -49,6 +243,342 @@ num_workers = 4
 
 #<<<YOUR CODE HERE>>>
 
+def train(model,criterion,optimizer,train_loader,valid_loader,save_file_name,
+          max_epochs_stop=3,n_epochs=20,print_every=1):
+    """Train a PyTorch Model
+
+    Params
+    --------
+        model (PyTorch model): cnn to train
+        criterion (PyTorch loss): objective to minimize
+        optimizer (PyTorch optimizier): optimizer to compute gradients of model parameters
+        train_loader (PyTorch dataloader): training dataloader to iterate through
+        valid_loader (PyTorch dataloader): validation dataloader used for early stopping
+        save_file_name (str ending in '.pt'): file path to save the model state dict
+        max_epochs_stop (int): maximum number of epochs with no improvement in validation loss for early stopping
+        n_epochs (int): maximum number of training epochs
+        print_every (int): frequency of epochs to print training stats
+
+    Returns
+    --------
+        model (PyTorch model): trained cnn with best weights
+        history (DataFrame): history of train and validation loss and accuracy
+    """
+
+    # Early stopping intialization
+    epochs_no_improve = 0
+    valid_loss_min = np.Inf
+
+    valid_max_acc = 0
+    history = []
+
+    # Number of epochs already trained (if using loaded in model weights)
+    try:
+        print(f'Model has been trained for: {model.epochs} epochs.\n')
+    except:
+        model.epochs = 0
+        print(f'Starting Training from Scratch.\n')
+
+    overall_start = timer()
+
+    # Main loop
+    for epoch in range(n_epochs):
+
+        # keep track of training and validation loss each epoch
+        train_loss = 0.0
+        valid_loss = 0.0
+
+        train_acc = 0
+        valid_acc = 0
+
+        # Set to training
+        model.train()
+        start = timer()
+
+        # Training loop
+        for ii, (data, target) in enumerate(train_loader):
+            # Tensors to gpu
+            if train_on_gpu:
+                data, target = data.cuda(), target.cuda()
+
+            # Clear gradients
+            optimizer.zero_grad()
+            # Predicted outputs are log probabilities
+            output = model(data)
+
+            # Loss and backpropagation of gradients
+            loss = criterion(output, target)
+            loss.backward()
+
+            # Update the parameters
+            optimizer.step()
+
+            # Track train loss by multiplying average loss by number of examples in batch
+            train_loss += loss.item() * data.size(0)
+
+            # Calculate accuracy by finding max log probability
+            _, pred = torch.max(output, dim=1)
+            correct_tensor = pred.eq(target.data.view_as(pred))
+            # Need to convert correct tensor from int to float to average
+            accuracy = torch.mean(correct_tensor.type(torch.FloatTensor))
+            # Multiply average accuracy times the number of examples in batch
+            train_acc += accuracy.item() * data.size(0)
+
+            # Track training progress
+            print(
+                f'Epoch: {epoch}\t{100 * (ii + 1) / len(train_loader):.2f}% complete. {timer() - start:.2f} seconds elapsed in epoch.',
+                end='\r')
+
+        # After training loops ends, start validation
+        else:
+            model.epochs += 1
+
+            # Don't need to keep track of gradients
+            with torch.no_grad():
+                # Set to evaluation mode
+                model.eval()
+
+                # Validation loop
+                for data, target in valid_loader:
+                    # Tensors to gpu
+                    if train_on_gpu:
+                        data, target = data.cuda(), target.cuda()
+
+                    # Forward pass
+                    output = model(data)
+
+                    # Validation loss
+                    loss = criterion(output, target)
+                    # Multiply average loss times the number of examples in batch
+                    valid_loss += loss.item() * data.size(0)
+
+                    # Calculate validation accuracy
+                    _, pred = torch.max(output, dim=1)
+                    correct_tensor = pred.eq(target.data.view_as(pred))
+                    accuracy = torch.mean(
+                        correct_tensor.type(torch.FloatTensor))
+                    # Multiply average accuracy times the number of examples
+                    valid_acc += accuracy.item() * data.size(0)
+
+                # Calculate average losses
+                train_loss = train_loss / len(train_loader.dataset)
+                valid_loss = valid_loss / len(valid_loader.dataset)
+
+                # Calculate average accuracy
+                train_acc = train_acc / len(train_loader.dataset)
+                valid_acc = valid_acc / len(valid_loader.dataset)
+
+                history.append([train_loss, valid_loss, train_acc, valid_acc])
+
+                # Print training and validation results
+                if (epoch + 1) % print_every == 0:
+                    print(
+                        f'\nEpoch: {epoch} \tTraining Loss: {train_loss:.4f} \tValidation Loss: {valid_loss:.4f}'
+                    )
+                    print(
+                        f'\t\tTraining Accuracy: {100 * train_acc:.2f}%\t Validation Accuracy: {100 * valid_acc:.2f}%'
+                    )
+
+                # Save the model if validation loss decreases
+                if valid_loss < valid_loss_min:
+                    # Save model
+                    torch.save(model.state_dict(), save_file_name)
+                    # Track improvement
+                    epochs_no_improve = 0
+                    valid_loss_min = valid_loss
+                    valid_best_acc = valid_acc
+                    best_epoch = epoch
+
+                # Otherwise increment count of epochs with no improvement
+                else:
+                    epochs_no_improve += 1
+                    # Trigger early stopping
+                    if epochs_no_improve >= max_epochs_stop:
+                        print(
+                            f'\nEarly Stopping! Total epochs: {epoch}. Best epoch: {best_epoch} with loss: {valid_loss_min:.2f} and acc: {100 * valid_acc:.2f}%'
+                        )
+                        total_time = timer() - overall_start
+                        print(
+                            f'{total_time:.2f} total seconds elapsed. {total_time / (epoch+1):.2f} seconds per epoch.'
+                        )
+
+                        # Load the best state dict
+                        model.load_state_dict(torch.load(save_file_name))
+                        # Attach the optimizer
+                        model.optimizer = optimizer
+
+                        # Format history
+                        history = pd.DataFrame(
+                            history,
+                            columns=[
+                                'train_loss', 'valid_loss', 'train_acc',
+                                'valid_acc'
+                            ])
+                        return model, history
+
+    # Attach the optimizer
+    model.optimizer = optimizer
+    # Record overall time and print out stats
+    total_time = timer() - overall_start
+    print(
+        f'\nBest epoch: {best_epoch} with loss: {valid_loss_min:.2f} and acc: {100 * valid_acc:.2f}%'
+    )
+    print(
+        f'{total_time:.2f} total seconds elapsed. {total_time / (epoch):.2f} seconds per epoch.'
+    )
+    # Format history
+    history = pd.DataFrame(
+        history,
+        columns=['train_loss', 'valid_loss', 'train_acc', 'valid_acc'])
+    return model, history        
+        
+model, history = train(
+    model,
+    criterion,
+    optimizer,
+    dataloaders['train'],
+    dataloaders['val'],
+    save_file_name=save_file_name,
+    max_epochs_stop=5,
+    n_epochs=30,
+    print_every=1)
+
+
+plt.figure(figsize=(8, 6))
+for c in ['train_loss', 'valid_loss']:
+    plt.plot(
+        history[c], label=c)
+plt.legend()
+plt.xlabel('Epoch')
+plt.ylabel('Average Negative Log Likelihood')
+plt.title('Training and Validation Losses')
+
+plt.figure(figsize=(8, 6))
+for c in ['train_acc', 'valid_acc']:
+    plt.plot(
+        100 * history[c], label=c)
+plt.legend()
+plt.xlabel('Epoch')
+plt.ylabel('Average Accuracy')
+plt.title('Training and Validation Accuracy')
+
+
+def save_checkpoint(model, path):
+    """Save a PyTorch model checkpoint
+
+    Params
+    --------
+        model (PyTorch model): model to save
+        path (str): location to save model. Must start with `model_name-` and end in '.pth'
+
+    Returns
+    --------
+        None, save the `model` to `path`
+
+    """
+
+    model_name = path.split('-')[0]
+    assert (model_name in ['vgg16', 'resnet50'
+                           ]), "Path must have the correct model name"
+
+    # Basic details
+    checkpoint = {
+        'class_to_idx': model.class_to_idx,
+        'idx_to_class': model.idx_to_class,
+        'epochs': model.epochs,
+    }
+
+    # Extract the final classifier and the state dictionary
+    if model_name == 'vgg16':
+        # Check to see if model was parallelized
+        if multi_gpu:
+            checkpoint['classifier'] = model.module.classifier
+            checkpoint['state_dict'] = model.module.state_dict()
+        else:
+            checkpoint['classifier'] = model.classifier
+            checkpoint['state_dict'] = model.state_dict()
+
+    elif model_name == 'resnet50':
+        if multi_gpu:
+            checkpoint['fc'] = model.module.fc
+            checkpoint['state_dict'] = model.module.state_dict()
+        else:
+            checkpoint['fc'] = model.fc
+            checkpoint['state_dict'] = model.state_dict()
+
+    # Add the optimizer
+    checkpoint['optimizer'] = model.optimizer
+    checkpoint['optimizer_state_dict'] = model.optimizer.state_dict()
+
+    # Save the data to the path
+    torch.save(checkpoint, path)
+
+save_checkpoint(model, path=checkpoint_path)
+
+def load_checkpoint(path):
+    """Load a PyTorch model checkpoint
+
+    Params
+    --------
+        path (str): saved model checkpoint. Must start with `model_name-` and end in '.pth'
+
+    Returns
+    --------
+        None, save the `model` to `path`
+
+    """
+
+    # Get the model name
+    model_name = path.split('-')[0]
+    assert (model_name in ['vgg16', 'resnet50'
+                           ]), "Path must have the correct model name"
+
+    # Load in checkpoint
+    checkpoint = torch.load(path)
+
+    if model_name == 'vgg16':
+        model = models.vgg16(pretrained=True)
+        # Make sure to set parameters as not trainable
+        for param in model.parameters():
+            param.requires_grad = False
+        model.classifier = checkpoint['classifier']
+
+
+    # Load in the state dict
+    model.load_state_dict(checkpoint['state_dict'])
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f'{total_params:,} total parameters.')
+    total_trainable_params = sum(
+        p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'{total_trainable_params:,} total gradient parameters.')
+
+    # Move to gpu
+    if train_on_gpu:
+        model = model.to('cuda')
+
+    # Model basics
+    model.class_to_idx = checkpoint['class_to_idx']
+    model.idx_to_class = checkpoint['idx_to_class']
+    model.epochs = checkpoint['epochs']
+
+    # Optimizer
+    optimizer = checkpoint['optimizer']
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    return model, optimizer
+
+
+model, optimizer = load_checkpoint(path=checkpoint_path)
+
+#if multi_gpu:
+#    summary(model.module, input_size=(3, 224, 224), batch_size=batch_size)
+#else:
+#    summary(model, input_size=(3, 224, 224), batch_size=batch_size)
+    
+model, history = train(model,criterion,optimizer,dataloaders['train'],dataloaders['val'],
+                       save_file_name=save_file_name,max_epochs_stop=5,n_epochs=30,print_every=1)
+    
 
 # When you have all the parameters in place, uncomment these to use the functions imported above
 #def main():
